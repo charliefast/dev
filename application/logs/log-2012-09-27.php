745e2f181242d17653d83147aafc855d221d2398
Carina on bowser <carina.m.ekholm@gmail.com>
2012-09-28 07:52:19 +0200

    config

---
+++ application/logs/log-2012-09-27.php
@@ -0,0 +1,266 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2012-09-27 00:41:04 --> Config Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Hooks Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 00:41:04 --> URI Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Router Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Output Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Security Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Input Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 00:41:04 --> Language Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Loader Class Initialized
+DEBUG - 2012-09-27 00:41:04 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 00:41:04 --> Database Driver Class Initialized
+ERROR - 2012-09-27 00:41:04 --> Severity: Notice  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
+ C:\wamp\www\dev\system\database\drivers\mysql\mysql_driver.php 91
+DEBUG - 2012-09-27 00:41:05 --> Session Class Initialized
+DEBUG - 2012-09-27 00:41:05 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 00:41:05 --> A session cookie was not found.
+DEBUG - 2012-09-27 00:41:05 --> Session routines successfully run
+DEBUG - 2012-09-27 00:41:05 --> User Agent Class Initialized
+DEBUG - 2012-09-27 00:41:05 --> Template class Initialized
+DEBUG - 2012-09-27 00:41:05 --> Controller Class Initialized
+DEBUG - 2012-09-27 00:41:05 --> Helper loaded: form_helper
+DEBUG - 2012-09-27 00:41:05 --> Form Validation Class Initialized
+DEBUG - 2012-09-27 00:41:05 --> Language file loaded: language/swedish/form_validation_lang.php
+DEBUG - 2012-09-27 00:41:05 --> Model Class Initialized
+DEBUG - 2012-09-27 00:41:05 --> Model Class Initialized
+DEBUG - 2012-09-27 00:41:05 --> File loaded: application/views/registration_view.php
+DEBUG - 2012-09-27 00:41:05 --> Final output sent to browser
+DEBUG - 2012-09-27 00:41:05 --> Total execution time: 0.4026
+DEBUG - 2012-09-27 13:12:49 --> Config Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:12:49 --> URI Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Router Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Output Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Security Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Input Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:12:49 --> Language Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Loader Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 13:12:49 --> Database Driver Class Initialized
+ERROR - 2012-09-27 13:12:49 --> Severity: Notice  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
+ C:\wamp\www\dev\system\database\drivers\mysql\mysql_driver.php 91
+DEBUG - 2012-09-27 13:12:49 --> Session Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 13:12:49 --> A session cookie was not found.
+DEBUG - 2012-09-27 13:12:49 --> Session routines successfully run
+DEBUG - 2012-09-27 13:12:49 --> User Agent Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Template class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Controller Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Helper loaded: form_helper
+DEBUG - 2012-09-27 13:12:49 --> Form Validation Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Language file loaded: language/swedish/form_validation_lang.php
+DEBUG - 2012-09-27 13:12:49 --> Model Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> Model Class Initialized
+DEBUG - 2012-09-27 13:12:49 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:49 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:49 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:49 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:49 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:49 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:49 --> File loaded: application/views/registration_view.php
+DEBUG - 2012-09-27 13:12:49 --> Final output sent to browser
+DEBUG - 2012-09-27 13:12:49 --> Total execution time: 0.1471
+DEBUG - 2012-09-27 13:12:59 --> Config Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:12:59 --> URI Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Router Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Output Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Security Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Input Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:12:59 --> Language Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Loader Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 13:12:59 --> Database Driver Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Session Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 13:12:59 --> Session routines successfully run
+DEBUG - 2012-09-27 13:12:59 --> User Agent Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Template class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Controller Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Helper loaded: form_helper
+DEBUG - 2012-09-27 13:12:59 --> Form Validation Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Language file loaded: language/swedish/form_validation_lang.php
+DEBUG - 2012-09-27 13:12:59 --> Model Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Model Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:59 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:59 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:59 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:59 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:59 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:12:59 --> Email Class Initialized
+DEBUG - 2012-09-27 13:12:59 --> Final output sent to browser
+DEBUG - 2012-09-27 13:12:59 --> Total execution time: 0.2762
+DEBUG - 2012-09-27 13:13:03 --> Config Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:13:03 --> URI Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Router Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Output Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Security Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Input Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:13:03 --> Language Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Loader Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 13:13:03 --> Database Driver Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Session Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 13:13:03 --> Session routines successfully run
+DEBUG - 2012-09-27 13:13:03 --> User Agent Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Template class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Controller Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Helper loaded: form_helper
+DEBUG - 2012-09-27 13:13:03 --> Form Validation Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Language file loaded: language/swedish/form_validation_lang.php
+DEBUG - 2012-09-27 13:13:03 --> Model Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Model Class Initialized
+DEBUG - 2012-09-27 13:13:03 --> Final output sent to browser
+DEBUG - 2012-09-27 13:13:03 --> Total execution time: 0.2171
+DEBUG - 2012-09-27 13:13:05 --> Config Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:13:05 --> URI Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Router Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Output Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Security Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Input Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:13:05 --> Language Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Loader Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 13:13:05 --> Database Driver Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Session Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 13:13:05 --> Session routines successfully run
+DEBUG - 2012-09-27 13:13:05 --> User Agent Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Template class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Controller Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Helper loaded: form_helper
+DEBUG - 2012-09-27 13:13:05 --> File loaded: application/views/login_view.php
+DEBUG - 2012-09-27 13:13:05 --> Final output sent to browser
+DEBUG - 2012-09-27 13:13:05 --> Total execution time: 0.1033
+DEBUG - 2012-09-27 13:13:05 --> Config Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:13:05 --> URI Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Router Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Output Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Security Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Input Class Initialized
+DEBUG - 2012-09-27 13:13:05 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:13:05 --> Language Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Config Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:13:14 --> URI Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Router Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Output Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Security Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Input Class Initialized
+DEBUG - 2012-09-27 13:13:14 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:13:14 --> Language Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Config Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:13:18 --> URI Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Router Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Output Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Security Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Input Class Initialized
+DEBUG - 2012-09-27 13:13:18 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:13:18 --> Language Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Config Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:13:22 --> URI Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Router Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Output Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Security Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Input Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:13:22 --> Language Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Loader Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 13:13:22 --> Database Driver Class Initialized
+ERROR - 2012-09-27 13:13:22 --> Severity: Notice  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
+ C:\wamp\www\dev\system\database\drivers\mysql\mysql_driver.php 91
+DEBUG - 2012-09-27 13:13:22 --> Session Class Initialized
+DEBUG - 2012-09-27 13:13:22 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 13:13:22 --> Session routines successfully run
+DEBUG - 2012-09-27 13:13:23 --> User Agent Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Template class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Controller Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Model Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Model Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Helper loaded: form_helper
+DEBUG - 2012-09-27 13:13:23 --> Form Validation Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Language file loaded: language/swedish/form_validation_lang.php
+DEBUG - 2012-09-27 13:13:23 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:13:23 --> XSS Filtering completed
+DEBUG - 2012-09-27 13:13:23 --> Config Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Hooks Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 13:13:23 --> URI Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Router Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Output Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Security Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Input Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 13:13:23 --> Language Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Loader Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 13:13:23 --> Database Driver Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Session Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 13:13:23 --> Session routines successfully run
+DEBUG - 2012-09-27 13:13:23 --> User Agent Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Template class Initialized
+DEBUG - 2012-09-27 13:13:23 --> Controller Class Initialized
+DEBUG - 2012-09-27 13:13:23 --> File loaded: application/views/home_view.php
+DEBUG - 2012-09-27 13:13:23 --> Final output sent to browser
+DEBUG - 2012-09-27 13:13:23 --> Total execution time: 0.1125
+DEBUG - 2012-09-27 18:03:36 --> Config Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Hooks Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Utf8 Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> UTF-8 Support Enabled
+DEBUG - 2012-09-27 18:03:36 --> URI Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Router Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> No URI present. Default controller set.
+DEBUG - 2012-09-27 18:03:36 --> Output Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Security Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Input Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-09-27 18:03:36 --> Language Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Loader Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Helper loaded: url_helper
+DEBUG - 2012-09-27 18:03:36 --> Database Driver Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Session Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Helper loaded: string_helper
+DEBUG - 2012-09-27 18:03:36 --> A session cookie was not found.
+DEBUG - 2012-09-27 18:03:36 --> Session routines successfully run
+DEBUG - 2012-09-27 18:03:36 --> User Agent Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Template class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Controller Class Initialized
+DEBUG - 2012-09-27 18:03:36 --> Helper loaded: form_helper
+DEBUG - 2012-09-27 18:03:36 --> File loaded: application/views/login_view.php
+DEBUG - 2012-09-27 18:03:36 --> Final output sent to browser
+DEBUG - 2012-09-27 18:03:36 --> Total execution time: 0.2009
