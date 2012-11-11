48d81cdefedb28a0dcf93deac26ae5a83d9f8f1c
Hector Romo <hectorromo@Hectors-MacBook-Pro.local>
2012-11-11 01:34:07 +0100

    Small config changes

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -19,7 +19,6 @@
 
   <!-- CSS
   ================================================== -->
-  <link rel="stylesheet" href="<?php echo base_url();?>css/reset.css">
   <link rel="stylesheet" href="<?php echo base_url();?>css/main.css" type="text/css" media="screen" />
   
   
