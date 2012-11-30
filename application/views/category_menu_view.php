d768899b956fddad11ab12c6d67a536e10a6c0e4
Hector Romo <hectorromo@Hectors-MacBook-Pro.local>
2012-11-30 12:41:00 +0100

    responsive fixes, item fixes

--- application/views/category_menu_view.php
+++ application/views/category_menu_view.php
@@ -1,9 +1,9 @@
-<div class="container">
-	<ul id="categoryList" class="nav nav-pills">
-	<?php foreach ($url as $item):?>
-
-	<li><?php echo $item;?></li>
-
-	<?php endforeach;?>
-	</ul>
+<div class="container-fluid">
+	<div class="row-fluid">
+		<ul id="categoryList" class="nav nav-pills">
+			<?php foreach ($url as $item):?>
+			<li><?php echo $item;?></li>
+			<?php endforeach;?>
+		</ul>
+	</div>
 </div>
--- application/views/item_view.php
+++ application/views/item_view.php
@@ -5,6 +5,16 @@
 		<div class="span12">
 			<form id="searchForm">
 				<input id="search" type="text" placeholder="Sök i det här fältet">
+
+				<label for="categories">Välj kategori</label>
+				<select id="categories">
+					<option>Välj alla</option>
+					<option>Hushåll</option>
+					<option>Hushåll</option>
+					<option>Hushåll</option>
+				</select>
+
+
 				<input id="submit" type="submit" class="btn btn-primary" value="sök">
 			</form>
 		</div>
